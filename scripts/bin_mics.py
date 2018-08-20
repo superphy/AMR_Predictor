3ae28986e888a2132764a32f62357a3f4acb589e
JanMoat <jmoat@sfu.ca>
2018-08-20 08:59:29 -0600

    changed for tsv

--- scripts/bin_mics.py
+++ scripts/bin_mics.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 """bin_mics.py
@@ -10,6 +9,7 @@ import os
 import logging
 import numpy as np
 import pandas as pd
+import yaml
 import sys
 
 from dotenv import find_dotenv, load_dotenv
@@ -25,62 +25,6 @@ __maintainer__ = "Matthew Whiteside"
 __email__ = "matthew.whiteside@phac-aspc.gc.ca"
 
 # Manually define MIC ranges due to mixing of different systems
-mic_ranges = {
-    'MIC_AMP': {
-        'top': '>32.0000',
-        'bottom': '<=1.0000',
-    },
-    'MIC_AMC': {
-        'top': '>32.0000',
-        'bottom': '<=1.0000',
-    },
-    'MIC_FOX': {
-        'top': '>32.0000',
-        'bottom': '1.0000',
-    },
-    'MIC_CRO': {
-        'top': '64.0000',
-        'bottom': '<=0.2500',
-    },
-    'MIC_TIO': {
-        'top': '>8.0000',
-        'bottom': '0.2500',
-    },
-    'MIC_GEN': {
-        'top': '>16.0000',
-        'bottom': '<=0.2500',
-    },
-    'MIC_FIS': {
-        'top': '>256.0000',
-        'bottom': '<=16.0000',
-    },
-    'MIC_SXT': {
-        'top': '>64.0000',
-        'bottom': '<=0.1250',
-    },
-    'MIC_AZM': {
-        'top': '>16.0000',
-        'bottom': '<=1.0000',
-    },
-    'MIC_CHL': {
-        'top': '>32.0000',
-        'bottom': '<=2.0000',
-    },
-    'MIC_CIP': {
-        'top': '>4.0000',
-        'bottom': '<=0.0150',
-    },
-    'MIC_NAL': {
-        'top': '>32.0000',
-        'bottom': '1.0000',
-    },
-    'MIC_TET': {
-        'top': '>32.0000',
-        'bottom': '<=4.0000',
-    },
-
-}
-'''
 mic_ranges = {
     'MIC_AMP': {
         'top': '>=32.0000',
@@ -134,38 +78,64 @@ mic_ranges = {
         'top': '>32.0000',
         'bottom': '<=4.0000',
     },
-}
-'''
 
-def main(excel_filepath):
+}
+
+
+def main(excel_filepath, class_label_filepath=None):
     """ Runs data processing scripts to turn MIC text values from Excel input data
         into class categories
+        There are two modes
+            1. Only max and min are defined, all other observed MIC classes are assigned a bin provided
+            they are not incompatible (i.e. >8 in a panel where the MIC values range from 1 - 32)
+            2. All class labels are defined, observed MIC classes in data are mapped to these classes. An error
+            is thrown if no mapped classes is found. To use this version, provde a class_label_filepath above.
+            The class_label file will outline all classes in YAML format:
+                AMP:
+                    - 0.5
+                    - 1.0
+                    - 2.0
+                    - ...
+                - or -
+                AMP: [0.5, 1.0, 2.0, ...]
     Args:
         excel_filepath: Metadata Excel file. MIC columns will have prefix 'MIC_'
+        class_label_filepath: If this is provided, the class labels will be loaded, instead of inferred from data
     """
+
     logger = logging.getLogger(__name__)
     logger.info('MIC binning')
 
-    # drugs = ["MIC_AMP", "MIC_AMC", "MIC_FOX", "MIC_CRO", "MIC_TIO", "MIC_GEN",
-    #     "MIC_FIS", "MIC_SXT", "MIC_AZM", "MIC_CHL", "MIC_CIP", "MIC_NAL", "MIC_TET"]
-    # convert = { key: lambda x: str(x) for key in drugs }
-   
-    # micsdf = pd.read_csv(excel_filepath, sep='\t', usecols=['run'] + drugs, skip_footer=1, skip_blank_lines=False, converters=convert)
-    # micsdf = micsdf.set_index('run')
+    #micsdf = pd.read_excel(excel_filepath)
+    micsdf = pd.read_csv(excel_filepath, sep='\t')
+    print(micsdf)
 
-    micsdf = pd.read_excel(excel_filepath)
-    micsdf = micsdf[["run","MIC_AMP", "MIC_AMC", "MIC_FOX", "MIC_CRO", "MIC_TIO", "MIC_GEN", "MIC_FIS", "MIC_SXT", "MIC_AZM", "MIC_CHL", "MIC_CIP", "MIC_NAL", "MIC_TET"]]
+    micsdf = micsdf[
+        ["run", "MIC_AMP", "MIC_AMC", "MIC_FOX", "MIC_CRO", "MIC_TIO", "MIC_GEN", "MIC_FIS", "MIC_SXT", "MIC_AZM",
+         "MIC_CHL", "MIC_CIP", "MIC_NAL", "MIC_TET"]]
+
     micsdf = micsdf.set_index('run')
+
+    # micsdf = micsdf[
+    #     ["SANumber", "MIC_AMP", "MIC_AMC"]]
+    # micsdf = micsdf.set_index('SANumber')
+
+    mic_class_labels = None
+    if class_label_filepath:
+        logger.debug('Using classes defined in {}'.format(class_label_filepath))
+        with open(class_label_filepath, 'r') as infh:
+            mic_class_labels = yaml.load(infh)
 
     classes = {}
     class_orders = {}
     for col in micsdf:
         logger.debug('Creating MIC panel for {}'.format(col))
-        class_labels, class_order = bin(micsdf[col], col)
-
-        class_order = consolidate_bins(class_order)
-
         drug = col.replace('MIC_', '')
+        if mic_class_labels:
+            class_labels, class_order = transform(micsdf[col], mic_class_labels[drug])
+        else:
+            class_labels, class_order = bin(micsdf[col], col)
+
         classes[drug] = pd.Series(class_labels, index=micsdf.index)
         class_orders[drug] = class_order
 
@@ -173,60 +143,15 @@ def main(excel_filepath):
 
     c = pd.DataFrame(classes)
 
-    if not os.path.exists(os.path.abspath(os.path.curdir)+'/amr_data'):
-        os.mkdir(os.path.abspath(os.path.curdir)+'amr_data')
+
     cfile  = os.path.abspath(os.path.curdir)+"/amr_data/mic_class_dataframe.pkl"#os.path.join(data_dir, 'interim', 'mic_class_dataframe.pkl')
     cofile = os.path.abspath(os.path.curdir)+"/amr_data/mic_class_order_dict.pkl"#os.path.join(data_dir, 'interim', 'mic_class_order_dict.pkl')
+    #cfile = snakemake.output[0]
+    #cofile = snakemake.output[1]
     joblib.dump(c, cfile)
     joblib.dump(class_orders, cofile)
 
-def consolidate_bins(bins):
-    #print("before",bins)
-    index = 0
-    while index < len(bins)-1:
-        #a = int(''.join(filter(str.isdigit, bins[index])))
-        #b = int(''.join(filter(str.isdigit, bins[index+1])))
 
-        a = bins[index].split('=')[-1]
-        a = bins[index].split('<')[-1]
-        a = bins[index].split('>')[-1]
-
-        b = bins[index+1].split('=')[-1]
-        b = bins[index+1].split('<')[-1]
-        b = bins[index+1].split('>')[-1]
-
-        #print(a,b)
-
-        if a==b:
-            #bins = bins.delete(index)
-            del bins[index]
-            if index == 0:
-                bins[index] = "<="+str(a)
-            elif index == len(bins)-1:
-                bins[index] = ">="+str(a)
-        index+=1
-    print('MIC values will be mapped to: ', bins)
-    #print("after",bins)
-    return bins
-
-'''
-    for col in micsdf:
-        new_bins = []
-        # Get the column index of the drug
-        col_index = micsdf.columns.get_loc(col)
-        # Set invalid entries to be NaN for easy deletion
-        for index, row in micsdf.iterrows():
-            x =row[col_index]
-            x = str(x)
-            if x !='nan' and x != '-': x = int(''.join(filter(str.isdigit, x)))
-            if x !='nan' and x != '-' and x not in new_bins:
-                new_bins.append(x)
-
-        new_bins = np.asarray(new_bins)
-        new_bins = np.sort(new_bins)
-
-        print(col, new_bins)
-'''
 
 def bin(mic_series, drug):
 
@@ -244,7 +169,7 @@ def bin(mic_series, drug):
     panel.set_range(mic_ranges[drug]['top'], mic_ranges[drug]['bottom'])
 
     # Iterate through MIC values and assign class labels
-    #logger.debug('MIC values will be mapped to: {}'.format(panel.class_labels))
+    logger.debug('MIC values will be mapped to: {}'.format(panel.class_labels))
     classes = []
     for m in mic_series:
         mgpl = MGPL(m)
@@ -254,7 +179,7 @@ def bin(mic_series, drug):
             classes.append(np.nan)
         else:
             if not mlabel in panel.class_mapping:
-                raise Exception('Mapping error')
+                raise Exception('Mapping error: class {} not found in MIC panel.'.format(mlabel))
             else:
                 classes.append(panel.class_mapping[mlabel])
 
@@ -262,6 +187,31 @@ def bin(mic_series, drug):
     return(classes, panel.class_labels)
 
 
+def transform(mic_series, class_labels):
+
+    logger = logging.getLogger(__name__)
+
+    panel = MICPanel()
+
+    # Initialize MIC bins
+    panel.build(class_labels)
+
+    # Iterate through MIC values and assign class labels
+    logger.debug('MIC values will be mapped to: {}'.format(panel.class_labels))
+    classes = []
+    i = 0
+    for m in mic_series:
+        i += 1
+        mlabel, isna = panel.transform(m)
+
+        if isna:
+            classes.append(np.nan)
+        else:
+            classes.append(mlabel)
+
+    logger.info("Invalid MICs found in dataset: {}".format(', '.join(panel.invalids)))
+
+    return (classes, panel.class_labels)
 
 
 if __name__ == '__main__':
@@ -272,8 +222,9 @@ if __name__ == '__main__':
     project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
     load_dotenv(find_dotenv())
 
-    #main(snakemake.input[0])
-    #print(snakemake.input[0])
-    #print(sys.argv[1])
-    main(sys.argv[1])
-    #main('amr_data/no_ecoli_GenotypicAMR_Master.xlsx')
+    # Check for class file
+    clfp = None
+    #if hasattr(snakemake.params, 'class_labels') and snakemake.params.class_labels:
+    #    clfp = snakemake.params.class_labels
+
+    main(sys.argv[1], clfp)
