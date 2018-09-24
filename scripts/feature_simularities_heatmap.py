bbc12cbd13ffe0eced9a078a4537ee833f1dd14e
RylanSteinkey <35366065+RylanSteinkey@users.noreply.github.com>
2018-09-24 15:06:54 -0600

    Update feature_simularities_heatmap.py

--- scripts/feature_simularities_heatmap.py
+++ scripts/feature_simularities_heatmap.py
@@ -6,22 +6,8 @@ from sklearn.externals import joblib
 from sklearn.feature_selection import SelectKBest, f_classif
 import concurrent.futures
 import os
+import sys
 
-"""
-This program takes in a 2d matrix, extracts the best features based on pvalue from f_classif,
-and returns another 2d matrix containing the number of differences between each set of features,
- example output:
-	col1 col2 col3
-col1  0	   3    5
-col2  3    0    7
-col3  5    7    0
-
-
-Matrix for testing:
-Kmer_matrix = np.asarray([[1,2,2,3,3],
-						  [2,2,2,3,3],
-						  [2,2,2,3,3]])
-"""
 def encode_categories(data, class_dict):
 	'''
 	Given a set of bin numbers (data), and a set of classes (class_dict),
@@ -40,55 +26,92 @@ def encode_categories(data, class_dict):
 		arry = np.append(arry,temp)
 	return arry
 
-def num_of_non_same(cols):
-	counter = 0
-	col1, col2 = cols
-	#print(col1, col2)
-	col1 = T_kmer_matrix[col1]
-	col2 = T_kmer_matrix[col2]
-	for i in range(len(col1)):
-		if (col1[i]!=col2[i]):
-			counter +=1
-	return counter
+def same_distro(col1, col2):
+	p = 0.01
+	#return (stats.ks_2samp(kmer_matrix[0,:], kmer_matrix[1,:])[1])<p
+	return 1
 
-def exact_same(cols):
-	col1, col2 = cols
-	#print(col1, col2)
-	col1 = T_kmer_matrix[col1]
-	col2 = T_kmer_matrix[col2]
-	#print("in exact_same with:", col1, col2)
+def exact_same(col1, col2):
 	for i in range(len(col1)):
 		if col1[i]!=col2[i]:
 			return 0
 	return 1
 
-drug = 'AMP'
-df = joblib.load((os.path.abspath(os.path.curdir)+"/non_grdi/amr_data/mic_class_dataframe.pkl")) # Matrix of experimental MIC values
-mic_class_dict = joblib.load((os.path.abspath(os.path.curdir)+"/non_grdi/amr_data/mic_class_order_dict.pkl")) # Matrix of classes for each drug
-kmer_matrix = np.load((os.path.abspath(os.path.curdir)+'/non_grdi/amr_data/'+drug+'/kmer_matrix.npy'))
-kmer_cols = np.load((os.path.abspath(os.path.curdir)+'/non_grdi/amr_data/'+drug+'/kmer_cols.npy'))
-kmer_rows_mic = np.load((os.path.abspath(os.path.curdir)+'/non_grdi/amr_data/'+drug+'/kmer_rows_mic.npy'))
-kmer_rows_mic = encode_categories(kmer_rows_mic, mic_class_dict[drug])
-fvals, pvals = f_classif(kmer_matrix, kmer_rows_mic)
-pcutoff = 10**(-45)
-fmask = pvals < pcutoff
-kmer_matrix = kmer_matrix[:,fmask]
-kmer_cols = kmer_cols[fmask]
+def num_of_non_same(col1, col2):
+	counter = 0
+	for i in range(len(col1)):
+		if (col1[i]!=col2[i]):
+			counter +=1
+	return counter
 
-T_kmer_matrix = kmer_matrix.transpose()
+if __name__ == "__main__":
+	df = joblib.load((os.path.abspath(os.path.curdir)+"/non_grdi/amr_data/mic_class_dataframe.pkl")) # Matrix of experimental MIC values
+	mic_class_dict = joblib.load((os.path.abspath(os.path.curdir)+"/non_grdi/amr_data/mic_class_order_dict.pkl")) # Matrix of classes for each drug
 
+	#drugs = ["AMP","AMC","AZM","CHL","CIP","CRO","FIS","FOX","GEN","NAL","SXT","TET","TIO"]
+	drugs = [sys.argv[1]]
 
+	for drug in drugs:
 
-heatmap_matrix=[]
-#Single row of comparasions, creates an array of tuples for passing into concurrent.futures
-srofc = np.zeros((T_kmer_matrix.shape[0],2), dtype = 'uint8')
-for i in range(T_kmer_matrix.shape[0]):
-	srofc[i][1] = i
-for row in range(T_kmer_matrix.shape[0]):
-	for j in range(T_kmer_matrix.shape[0]):
-		srofc[j][0] = row
-	with concurrent.futures.ThreadPoolExecutor(max_workers = 64) as executor:
-		heatmap_matrix.append(list(executor.map(num_of_non_same, srofc)))
+		print("*******",drug,"*******")
+		kmer_matrix = np.load((os.path.abspath(os.path.curdir)+'/non_grdi/amr_data/'+drug+'/kmer_matrix.npy'))
+		kmer_cols = np.load((os.path.abspath(os.path.curdir)+'/non_grdi/amr_data/'+drug+'/kmer_cols.npy'))
+		kmer_rows_mic = np.load((os.path.abspath(os.path.curdir)+'/non_grdi/amr_data/'+drug+'/kmer_rows_mic.npy'))
 
-np.save('no_dup_feats/'+drug+'_par_heatmap_matrix.npy', np.asarray(heatmap_matrix))
-np.save('no_dup_feats/'+drug+'_par_heatmap_kmer_cols.npy',kmer_cols)
+		kmer_rows_mic = encode_categories(kmer_rows_mic, mic_class_dict[drug])
+
+		#sk_obj = SelectKBest(f_classif, k=1000)
+		#kmer_matrix = sk_obj.fit_transform(kmer_matrix, kmer_rows_mic)
+
+		fvals, pvals = f_classif(kmer_matrix, kmer_rows_mic)
+
+		# use this loop to find a good cutoff for later
+
+		for i in range(50):
+			pcutoff = 10**(-i)
+			fmask = pvals < pcutoff
+			print(pcutoff, '->', np.sum(fmask))
+			if(np.sum(fmask) < 30000):
+				break
+
+		"""
+		pcutoff = 10**(-43)
+		if(drug == 'AZM'):
+			pcutoff = 10**(-25)
+		elif(drug == 'CIP'):
+			pcutoff = 10**(-32)
+		elif(drug == 'NAL'):
+			pcutoff = 10**(-37)
+		elif(drug == 'GEN'):
+			pcutoff = 10**(-39)
+		elif(drug == 'SXT'):
+			pcutoff = 10**(-21)
+		"""
+		pcutoff = pcutoff*10
+		fmask = pvals < pcutoff
+		print(pcutoff)
+		print('fmask:', np.sum(fmask))
+		#print("Before:", kmer_matrix.shape)
+
+		kmer_matrix = kmer_matrix[:,fmask]
+		kmer_cols = kmer_cols[fmask]
+
+		#kmer_matrix = [[1,2,2,3,3],[2,2,2,3,3],[2,2,2,3,3]]
+		#print("After:", kmer_matrix.shape)
+		#kmer_matrix = np.asarray(kmer_matrix)
+		#print(kmer_matrix)
+		heatmap_matrix = np.zeros((kmer_matrix.shape[1], kmer_matrix.shape[1]), dtype='uint32')
+		kmer_matrix = kmer_matrix.transpose()
+		copy_mask = np.zeros(kmer_matrix.shape[0])
+		copy_count = 0
+		break_counter = 0
+		for cnti, i in enumerate(kmer_matrix):
+			#print(i)
+			for cntj, j in enumerate(kmer_matrix):
+				if (cnti<cntj):
+					heatmap_matrix[cnti][cntj]=num_of_non_same(i,j)
+			#break_counter+=1
+			#if(break_counter==10):
+				#break
+		np.save('no_dup_feats/'+drug+'_heatmap_matrix.npy', heatmap_matrix)
+		np.save('no_dup_feats/'+drug+'_heatmap_kmer_cols.npy',kmer_cols)
